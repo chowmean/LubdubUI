d36a9c6ef9d177b1c45e6a5c0ee1c89ba756a843
chowmean <gauurvvvv@gmail.com>
2018-02-04 23:15:09 +0530

    adding functionality to show if the node is inactive

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -20,6 +20,19 @@ angular.module('myApp.dashboard', ['ngRoute'])
         }).success(function(result) {
             $scope.cpu_info = result.data
             $scope.total_host = result.data.length
+            $.each($scope.cpu_info,function(index,value){
+            	var a = moment(value['data']['cpu']['datetime']);
+				var b = moment();
+				var diff = b.diff(a, 'seconds');
+				if (diff-19800<120){
+					value['active'] = true;
+				}
+				else{
+					value['active'] = false;
+				}
+
+            })
+            console.log($scope.cpu_info);
         })
         .error(function(data) {
             ;
